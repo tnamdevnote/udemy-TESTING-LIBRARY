d2b98185f90aab9da50c46c7a77d460fa728fefe
Taek Been Nam <taekbeen93@gmail.com>
2024-01-16 16:28:53 -0500

    more tests for Color Button

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
 import './App.css';
 function App() {
+  const [isDisabled, setIsDisabled] = useState(false);
   const [btnColor, setBtnColor] = useState('red');
   const nextColor = btnColor === 'red' ? 'blue' : 'red';
 
   return (
-    <div>
-      <button className={btnColor} onClick={() => setBtnColor(nextColor)}>{`Change to ${nextColor}`}</button>
+    <div className="container">
+      <button className={isDisabled ? 'grey' : btnColor} disabled={isDisabled} onClick={() => setBtnColor(nextColor)}>
+        {`Change to ${nextColor}`}
+      </button>
+      <div>
+        <label htmlFor="disable-btn-checkbox">Disable button</label>
+        <input
+          type="checkbox"
+          id="disable-btn-checkbox"
+          defaultChecked={isDisabled}
+          onChange={(e) => setIsDisabled(e.target.checked)}
+        />
+      </div>
     </div>
   );
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { expect } from 'vitest';
 
-test('button starts with correct label and color', () => {
+test('button click flow', () => {
   // render component
   render(<App />);
   // checks if the button exists and if its text contains 'blue' text.
@@ -16,4 +17,53 @@ test('button starts with correct label and color', () => {
   expect(buttonElement).toHaveTextContent(/red/i);
 
   expect(buttonElement).toHaveClass('blue');
+});
+
+test('checkbox flow', () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i });
+
+  // check initial condition
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  // toggle the checkbox - check
+  fireEvent.click(checkboxElement);
+
+  // check disabled state
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveStyle({ 'background-color': 'rgb(128, 128, 128)' });
+
+  // toggle the checkbox - uncheck
+  fireEvent.click(checkboxElement);
+
+  // check enabled state
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveStyle({ 'background-color': 'rgb(255, 0, 0)' });
+});
+
+test('checkbox flow after button click', () => {
+  render(<App />);
+  // find elements
+  const buttonElement = screen.getByRole('button', { name: /blue/i });
+  const checkboxElement = screen.getByRole('checkbox', { name: /disable button/i });
+
+  fireEvent.click(buttonElement);
+
+  // toggle the checkbox - check
+  fireEvent.click(checkboxElement);
+
+  // check disabled state
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveStyle({ 'background-color': 'rgb(128, 128, 128)' });
+
+  // toggle the checkbox - uncheck
+  fireEvent.click(checkboxElement);
+
+  // check enabled state
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveStyle({ 'background-color': 'rgb(0, 0, 255)' });
 });
