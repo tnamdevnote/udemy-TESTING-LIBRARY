67eccaf5c5e3482231ca62c781ae770a9358255d
Taek Been Nam <taekbeen93@gmail.com>
2024-01-22 15:32:08 -0500

    tests for toppings subtotal

--- sundae-starter/src/pages/entry/ToppingOptions.jsx
+++ sundae-starter/src/pages/entry/ToppingOptions.jsx
@@ -1,9 +1,19 @@
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { useOrderDetailsContext } from '../context/OrderDetailsContext';
 
-export default function ToppingOptions({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetailsContext();
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, 'toppings');
+  };
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: 'center' }}>
       <img style={{ width: '75%' }} src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
   );
 }
--- sundae-starter/src/pages/entry/tests/TotalUpdates.test.jsx
+++ sundae-starter/src/pages/entry/tests/TotalUpdates.test.jsx
@@ -25,3 +25,32 @@ test('update subtotal when scoops change', async () => {
   await user.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update subtotal when toppings change', async () => {
+  // set up user and render Toppings components
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  // make sure subtotals starts out at $0.00
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  // Cherries
+  const cherriesInput = await screen.findByRole('checkbox', { name: 'Cherries' });
+  await user.click(cherriesInput);
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  // M&Ms
+  // There's no need to await since we know that the data is already loaded
+  const mnmInput = screen.getByRole('checkbox', { name: 'M&Ms' });
+  await user.click(mnmInput);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  // Hot Fudge
+  const hotFudgeInput = screen.getByRole('checkbox', { name: 'Hot fudge' });
+  await user.click(hotFudgeInput);
+  expect(toppingsSubtotal).toHaveTextContent('4.50');
+
+  await user.click(hotFudgeInput);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+});
