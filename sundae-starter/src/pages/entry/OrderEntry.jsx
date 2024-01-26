554c1d70ef93400d5bf2518e57d389d5de22d2d9
Taek Been Nam <taekbeen93@gmail.com>
2024-01-26 11:32:50 -0500

    test disabled orer button

--- sundae-starter/src/pages/entry/OrderEntry.jsx
+++ sundae-starter/src/pages/entry/OrderEntry.jsx
@@ -6,13 +6,17 @@ import { useOrderDetailsContext } from '../context/OrderDetailsContext';
 export default function OrderEntry({ setOrderPhase }) {
   const { totals } = useOrderDetailsContext();
 
+  const orderDisabled = totals.scoops === 0;
+
   return (
     <div>
       <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
-      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   );
 }
--- sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -5,6 +5,7 @@ import { server } from '../../../mocks/server';
 import { render, screen } from '../../../test-utils/testing-library-utils';
 import OrderEntry from '../OrderEntry';
 import { expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
@@ -20,4 +21,26 @@ test('handles error for scoops and toppings routes', async () => {
 
   const alerts = await screen.findAllByText('An unexpected error occurred. Please try again later.');
   expect(alerts).toHaveLength(2);
+});
+
+test('disable order button for no scoops', async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={vitest.fn()} />);
+
+  const scoopsSubtotal = screen.getByText('Scoops total: $', { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  const orderButton = screen.getByRole('button', { name: 'Order Sundae!' });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaScoop = await screen.findByRole('spinbutton', { name: 'Vanilla' });
+  await user.clear(vanillaScoop);
+  await user.type(vanillaScoop, '1');
+  expect(scoopsSubtotal).toHaveTextContent('2.00');
+  expect(orderButton).not.toBeDisabled();
+
+  await user.clear(vanillaScoop);
+  await user.type(vanillaScoop, '0');
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+  expect(orderButton).toBeDisabled();
 });
