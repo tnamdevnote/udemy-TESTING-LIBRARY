3f98586b54e73e5d99861074b5f50b55335309c5
Taek Been Nam <taekbeen93@gmail.com>
2024-01-26 12:37:57 -0500

    test valid scoop input part 1

--- sundae-starter/src/pages/entry/ScoopOptions.jsx
+++ sundae-starter/src/pages/entry/ScoopOptions.jsx
@@ -2,11 +2,23 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
 import { useOrderDetailsContext } from '../context/OrderDetailsContext';
+import { useState } from 'react';
 
 export default function ScoopOptions({ name, imagePath }) {
   const { updateItemCount } = useOrderDetailsContext();
+  const [isValid, setIsValid] = useState(true);
 
-  const handleChange = (e) => updateItemCount(name, parseInt(e.target.value), 'scoops');
+  const handleChange = (e) => {
+    const currentValue = e.target.value;
+
+    const currentValueFloat = parseFloat(currentValue);
+    const valueIsValid =
+      0 <= currentValueFloat && currentValueFloat <= 10 && Math.floor(currentValueFloat) === currentValueFloat;
+
+    setIsValid(valueIsValid);
+
+    updateItemCount(name, parseInt(currentValue), 'scoops');
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img style={{ width: '75%' }} src={`http://localhost:3030/${imagePath}`} alt={`${name} scoop`} />
@@ -15,7 +27,7 @@ export default function ScoopOptions({ name, imagePath }) {
           {name}
         </Form.Label>
         <Col xs="5" style={{ textAlign: 'left' }}>
-          <Form.Control type="number" defaultValue={0} onChange={handleChange} />
+          <Form.Control type="number" defaultValue={0} onChange={handleChange} isInvalid={!isValid} />
         </Col>
       </Form.Group>
     </Col>
